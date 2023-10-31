cd1a6e9840e240815773871355b1ca097af612dc
haturusinghe <s.haturusinghe99@gmail.com>
2023-10-31 15:02:39 +0530

    small fix

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 import ProjectList from "./ProjectList.js";
 
 const addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function () {
+addBtn.addEventListener("click", () => {
   window.location.href = "add.html";
 });
 
 const backBtn = document.getElementById("backToDashBtn");
-backBtn.addEventListener("click", function () {
+backBtn.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
