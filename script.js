cbf190f89edf536d35910bd3c5549604f6b23a38
haturusinghe <s.haturusinghe99@gmail.com>
2023-10-31 14:48:59 +0530

    nav buttons functions added

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 import ProjectList from "./ProjectList.js";
 
+document.addEventListener('DOMContentLoaded', function() {
+    const addBtn = document.getElementById('addBtn');
+    addBtn.addEventListener('click', function() {
+        window.location.href = 'add.html';
+    });
+    
+    const backBtn = document.getElementById('backToDashBtn');
+    backBtn.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+});
+
+
 const projectList = new ProjectList();
 
 projectList.init();
+
+
