f2bf01d89ed3a4febd191f36cdcc3ca4c6c67203
haturusinghe <s.haturusinghe99@gmail.com>
2023-10-31 15:00:45 +0530

    minor

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 import ProjectList from "./ProjectList.js";
 
-document.addEventListener('DOMContentLoaded', function() {
-    const addBtn = document.getElementById('addBtn');
-    addBtn.addEventListener('click', function() {
-        window.location.href = 'add.html';
-    });
-    
-    const backBtn = document.getElementById('backToDashBtn');
-    backBtn.addEventListener('click', function() {
-        window.location.href = 'index.html';
-    });
+const addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function () {
+  window.location.href = "add.html";
 });
 
+const backBtn = document.getElementById("backToDashBtn");
+backBtn.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
 
 const projectList = new ProjectList();
 
 projectList.init();
-
-
