13ced9b5a3e541c8b33e29c1f4d4b6c84688544e
haturusinghe <s.haturusinghe99@gmail.com>
2023-10-31 14:46:16 +0530

    added nav to list page

---
+++ dash-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const backBtn = document.getElementById('backToProjectListBtn');
+    backBtn.addEventListener('click', function() {
+        window.location.href = 'list.html';
+    });
+});
