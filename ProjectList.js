675b0365b6dcac73f3a7398fdce24aef7dbe0771
haturusinghe <s.haturusinghe99@gmail.com>
2023-10-31 19:10:31 +0530

    added delete button to end of row

--- ProjectList.js
+++ ProjectList.js
@@ -14,21 +14,27 @@ class ProjectList {
     });
   }
 
+  deleteProject(id){
+    return this.projectService.deleteProject(id);
+  }
+
   displayList(projectList) {
     projectTableBody.innerHTML = ``;
     if (projectList.length > 0) {
       projectList.forEach((project) => {
-        projectTableBody.innerHTML += `<tr><td>${project.id}</td><td>${
-          project.name
-        }</td><td>${project.revenue}</td><td>${
-          project.isCompleted == true ? "Completed" : "Ongoing"
-        }</td></tr>`;
+        projectTableBody.innerHTML += `
+        <tr>
+          <td>${project.id}</td>
+          <td>${project.name}</td>
+          <td>${project.revenue}</td>
+          <td>${project.isCompleted == true ? "Completed" : "Ongoing"}</td>
+          <td><td><button class="btn btn-danger" id="${project.id}"><i class="fas fa-trash"></i></button></td></td>
+        </tr>`;
       });
     } else {
       projectTableBody.innerHTML = `<tr class="text-center"><td colspan="4">No projects found</td></tr>`;
     }
   }
-
 }
 
 export default ProjectList;
