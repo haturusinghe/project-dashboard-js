3b6d4d3e70acf7755cb275e6d55a529305176d64
haturusinghe <s.haturusinghe99@gmail.com>
2023-10-31 19:11:38 +0530

    for every delete button attach an event listner for click and then on click call the deleteProject function in ProjectList calss

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ backBtn.addEventListener("click", () => {
 const projectList = new ProjectList();
 
 projectList.init();
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  const deleteButton = document.querySelectorAll(".btn-danger");
+  deleteButton.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      console.log("delete project with id", button.id);
+      alert(projectList.deleteProject(button.id));
+      window.location.href = "list.html";
+    });
+  });
+});
